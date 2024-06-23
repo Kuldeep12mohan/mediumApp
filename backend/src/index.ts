e6745722bc0068cf4eb0126a5ab1c024fe8be116
kuldeepkumar <mohankuldeep1234@gmail.com>
2024-06-23 17:53:57 +0530

    connection pool established

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,30 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post("/api/v1/signup",(c)=>
+{
+  return c.text("put vlog")
 })
+
+app.post("/api/v1/signin",(c)=>
+{
+  return c.text("put vlog")
+})
+
+app.post("/api/v1/blog",(c)=>
+{
+  return c.text("put vlog")
+})
+
+app.put("/api/v1/blog",(c)=>
+{
+  return c.text("put vlog")
+})
+
+app.get("/api/v1/blog:id",(c)=>
+{
+  return c.text("put vlog")
+})
+
 
 export default app
